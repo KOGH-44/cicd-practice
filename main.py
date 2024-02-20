6ad9c2eea7772e33de5f48f6f04a7263abc70d7b
Kim Keunhwan <76278512+KOGH-44@users.noreply.github.com>
2024-02-20 18:07:44 +0900

    'cicd'

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "Docker!!!!!"}
+    return {"I love ": "Docker!!!!!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q}~
